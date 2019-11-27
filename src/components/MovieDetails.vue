4f5b2c631262be33de37c32f51032cedc9fb79f6
Wilson <wc683@nyu.edu>
2019-11-26 21:32:14 -0500

    Resolved mapstate undefined issue!!

--- src/components/MovieDetails.vue
+++ src/components/MovieDetails.vue
@@ -2,12 +2,38 @@
   <div>
     <h3>Movie Details</h3>
     {{ id }}
+    {{ movieDetails }}
+    {{ trailer }}
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions, mapState } from "vuex";
+
 export default {
-  props: ["id"]
+  name: "MovieDetails",
+  props: ["id"],
+  methods: {
+    ...mapActions(["fetchMovieDetails"])
+  },
+  computed: {
+    // ...mapState(["movieDetails", "trailers"])
+
+    //...mapGetters(["movieDetails", "trailers"])
+
+    // movieDetails() {
+    //   return this.$store.state.movies.movieDetails;
+    // }
+
+    // Have to do it this way since our vuex store is in modules
+    ...mapState({
+      movieDetails: state => state.movies.movieDetails,
+      trailer: state => state.movies.trailer
+    })
+  },
+  created() {
+    this.fetchMovieDetails(this.id);
+  }
 };
 </script>
 
