905a531b28223a03c5cf82811e256472dadedd2f
Wilson <wc683@nyu.edu>
2019-12-11 01:54:00 -0500

    Fix lint errors with jquery not being defined

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/movies-app/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/movies-app/'
+    : '/',
+  lintOnSave: false
+}
